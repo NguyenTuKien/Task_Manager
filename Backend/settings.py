43d315f1a3e70557f91055c664c9fd3022b08f6e
NguyenTuKien <nguyentukien.81@gmail.com>
2025-09-15 07:19:31 +0700

    v1

--- Backend/settings.py
+++ Backend/settings.py
@@ -33,7 +33,8 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://0.0.0.0:3000',
-    'https://taskmanager-nguyentukien.vercel.app/'
+    'https://taskmanager-nguyentukien.vercel.app',
+    'https://taskmanager-nguyentukien.onrender.com',
 ]
 
 
@@ -139,7 +140,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://0.0.0.0:3000',
-    'https://taskmanager-nguyentukien.vercel.app'
+    'https://taskmanager-nguyentukien.vercel.app',
+    'https://taskmanager-nguyentukien.onrender.com',
 ]
 CORS_ALLOW_CREDENTIALS = True
 
